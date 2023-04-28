72c5d9e722c73b3ff55e7aac10591d33b42cd43f
jitender singh <jeetusinghkanyal@gmail.com>
2023-04-28 09:59:42 +0530

    completed Todo with Typeescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import InputField from './component/InputField';
+import { Todo } from './component/model';
+import TodoList from './component/TodoList';
 
-function App() {
+const  App:React.FC =()=> {
+  const [todo,setTodo]=useState<string>("");
+  const [todos,setTodos]=useState<Todo[]>([]);
+
+  const handleAdd=(e:React.FormEvent)=>{
+    e.preventDefault();
+
+    if(todo){
+      setTodos([...todos,{id:Date.now(),todo:todo,isDone:false}]);
+      setTodo("");
+    }
+    console.log(todos);
+  }
   return (
     <div className="App">
-     Heello world
+     <span className='heading'>Taskify</span>
+     <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
+     <TodoList todos={todos} setTodos={setTodos}/>
     </div>
   );
 }
